1ea0860ca1680c1ec56511925f5203406134674b
Nikolai Kostromitin <emailbin@yandex.ru>
2021-01-13 21:03:25 +0500

    Add empty lines

--- Board.cpp
+++ Board.cpp
@@ -7,6 +7,7 @@ void Board::createBoard(istream& in)
 	string line;
 	while (getline(in, line)) {
 		vector<shared_ptr<Cell>> row;
+		row.push_back(make_shared<Water>(WATER));
 		for (auto c : line) {
 			shared_ptr<Cell> cell;
 			if (c == WATER) {
@@ -17,8 +18,14 @@ void Board::createBoard(istream& in)
 			}
 			row.push_back(cell);
 		}
+		row.push_back(make_shared<Water>(WATER));
 		cells.push_back(row);
-	}
+	} // while
+
+	vector<shared_ptr<Cell>> row(cells[0].size(),
+		make_shared<Water>(WATER));
+	cells.insert(cells.begin(), row);
+	cells.push_back(row);
 }
 
 bool Board::isBoardCorrect() const
