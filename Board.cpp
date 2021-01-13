485075a5d66b400dc73734188a0c87b7a4eb9aef
Nikolai Kostromitin <emailbin@yandex.ru>
2021-01-13 20:44:24 +0500

    Add createBoard()

--- Board.cpp
+++ Board.cpp
@@ -1,7 +1,24 @@
 #include "Board.h"
+#include <string>
+using namespace std;
 
-void Board::createBoard(std::istream& in)
+void Board::createBoard(istream& in)
 {
+	string line;
+	while (getline(in, line)) {
+		vector<shared_ptr<Cell>> row;
+		for (auto c : line) {
+			shared_ptr<Cell> cell;
+			if (c == WATER) {
+				cell = make_shared<Water>(WATER);
+			}
+			else if (c == SHIP) {
+				cell = make_shared<ShipPart>(SHIP);
+			}
+			row.push_back(cell);
+		}
+		cells.push_back(row);
+	}
 }
 
 bool Board::isBoardCorrect() const
--- Board.h
+++ Board.h
@@ -19,6 +19,8 @@ public:
 		createShips();
 	}
 
+	enum { WATER = ' ', SHIP = '#' };
+
 private:
 	void createBoard(std::istream& in);
 	bool isBoardCorrect() const;
