5be308684295d5f07fda32111da73baa2ba402f6
Nikolai Kostromitin <emailbin@yandex.ru>
2021-01-13 20:20:36 +0500

    Create Board template

---
+++ Board.cpp
@@ -0,0 +1,14 @@
+#include "Board.h"
+
+void Board::createBoard(std::istream& in)
+{
+}
+
+bool Board::isBoardCorrect() const
+{
+	return true;
+}
+
+void Board::createShips()
+{
+}
---
+++ Board.h
@@ -0,0 +1,27 @@
+#pragma once
+#include <iostream>
+#include <memory>
+#include <stdexcept>
+#include <vector>
+#include "Cell.h"
+#include "Ship.h"
+
+class Board
+{
+	std::vector<std::vector<std::shared_ptr<Cell>>> cells;
+	std::vector<std::shared_ptr<Ship>> ships;
+public:
+	Board(std::istream& in) {
+		createBoard(in);
+		if (!isBoardCorrect()) {
+			throw std::invalid_argument("Board is not correct");
+		}
+		createShips();
+	}
+
+private:
+	void createBoard(std::istream& in);
+	bool isBoardCorrect() const;
+	void createShips();
+};
+
