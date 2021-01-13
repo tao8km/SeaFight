69eee78289f6da461f5355f107277a75cb45d095
Nikolai Kostromitin <emailbin@yandex.ru>
2021-01-13 21:25:03 +0500

    Add createShips()

--- Board.cpp
+++ Board.cpp
@@ -35,4 +35,21 @@ bool Board::isBoardCorrect() const
 
 void Board::createShips()
 {
+	for (int i = 1; i < cells.size() - 1; i++) {
+		for (int j = 1; j < cells[i].size() - 1; j++) {
+			if (cells[i][j]->getSymbol() == SHIP) {
+				std::vector<std::shared_ptr<ShipPart>> ship;
+				while (cells[i][j+1]->getSymbol() == SHIP) {
+					ship.push_back(make_shared<ShipPart>(cells[i][j]));
+					++j;
+				}
+				int k = i;
+				while (cells[k+1][j]->getSymbol() == SHIP) {
+					ship.push_back(make_shared<ShipPart>(cells[k][j]));
+					++k;
+				}
+				ships.push_back(make_shared<Ship>(ship));
+			}
+		}
+	}
 }
